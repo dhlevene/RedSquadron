1e3f17df4d9f0ce19baab291e97b37d6e81d07e4
Fazle Akbar <nocturnal.fazle@gmail.com>
2016-09-22 20:15:52 -0400

    final

--- PL0compiler/VirtualMachine.c
+++ PL0compiler/VirtualMachine.c
@@ -23,24 +23,24 @@ typedef struct{
 } instruction;
 
 //Function prototypes
-void execute(int instructions[][3], int stack[MAX_STACK_HEIGHT], int pc, int bp, int sp);
+void execute(int instructions[][4], int stack[MAX_STACK_HEIGHT], int pc, int bp, int sp);
 char* getOpCode(int op, int M);
-void translate(int instructions[][3], char* commands, int counter);
-instruction fetch(int instructions[][3], int counter);
+instruction fetch(int instructions[][4], int counter);
+int base(int l, int base, int stack[]);
+void printStack(int stack[], int sp, int cal);
 
 
-int main(){
+int main(int argc, char *argv[]){
 
-    instruction ir;
-
-    int instructions[MAX_CODE_LENGTH][3];
+    int instructions[MAX_CODE_LENGTH][4];
     int stack[MAX_STACK_HEIGHT];
-    int counterA=0, counterB=0, halt=0;
+    int counter=0;
     int sp=0, pc=0, bp=1;
     int i,j;
+    char* name;
 
 
-    FILE* fr = fopen(INPUT_FILE, "r");
+    FILE* fr = fopen(argv[1], "r");
 
     //Input not found
     if(fr==NULL)
@@ -48,7 +48,7 @@ int main(){
 
 
     for(i = 0; i < MAX_CODE_LENGTH; i++){
-        for(j=0; j < 3; j++){
+        for(j=0; j < 4; j++){
             instructions[i][j] = 0;
         }
     }
@@ -58,162 +58,42 @@ int main(){
     }
 
     printf("PL/0 code: \n\n");
-    while (fscanf(fr, "%d", &instructions[counterA][0]) != EOF){
-           fscanf(fr, "%d", &instructions[counterA][1]);
-           fscanf(fr, "%d", &instructions[counterA][2]);
+    while (fscanf(fr, "%d", &instructions[counter][0]) != EOF){
+           fscanf(fr, "%d", &instructions[counter][1]);
+           fscanf(fr, "%d", &instructions[counter][2]);
+           instructions[counter][3] = counter;
 
-        printf("%3d %d %s %d %2d\n", counterA,instructions[counterA][0],getOpCode(instructions[counterA][0],
-                               instructions[counterA][2]),instructions[counterA][1],instructions[counterA][2]);
-        counterA++;
+        name = getOpCode(instructions[counter][0], instructions[counter][2]);
+        printf("%3d  %s", counter, name);
+        if(name == "STO" || name == "CAL" || name == "LOD")
+            printf("    %d", instructions[counter][1]);
+        if(name != "RET" && name != "HLT")
+            printf("\t %2d", instructions[counter][2]);
+        printf("\n");
+        counter++;
     }
 
     fclose(fr);
 
-    //translate(instructions, commands, counter);
-
-    printf("\nExecution: \n\n");
-    //execute(instructions, stack, pc, bp, sp);
-    printf("pc\t bp\t sp\n", pc, bp, sp);
-    printf("%d\t %d\t %d\n\n", pc, bp, sp);
-
-    while(halt != 1)
-    {
-        ir = fetch(instructions, pc);
-        pc = pc+1;
-        if(ir.op == 1){
-            sp++;
-            stack[sp] = ir.m;
-        }
-
-        else if(ir.op == 2){
-
-            if(ir.m == 0){
-                sp = bp-1;
-                pc = stack[sp+4];
-                bp = stack[sp+3];
-            }
-            if(ir.m == 1){
-                stack[sp] = -stack[sp];
-            }
-            if(ir.m == 2){
-                sp--;
-                stack[sp] = stack[sp]+stack[sp+1];
-            }
-            if(ir.m == 3){
-                sp--;
-                stack[sp] = stack[sp]-stack[sp+1];
-            }
-            if(ir.m == 4){
-                sp--;
-                stack[sp] = stack[sp]*stack[sp+1];
-            }
-            if(ir.m == 5){
-                sp--;
-                stack[sp] = stack[sp]/stack[sp+1];
-            }
-            if(ir.m == 6){
-                stack[sp] = stack[sp]%2;
-            }
-            if(ir.m == 7){
-                sp--;
-                stack[sp] = stack[sp]%stack[sp+1];
-            }
-            if(ir.m == 8){
-                sp--;
-                stack[sp] = (stack[sp] == stack[sp+1]);
-            }
-            if(ir.m == 9){
-                sp--;
-                stack[sp] = (stack[sp] != stack[sp+1]);
-            }
-            if(ir.m == 10){
-                sp--;
-                stack[sp] = (stack[sp] < stack[sp+1]);
-            }
-            if(ir.m == 11){
-                sp--;
-                stack[sp] = (stack[sp] <= stack[sp+1]);
-            }
-            if(ir.m == 12){
-                sp--;
-                stack[sp] = (stack[sp] > stack[sp+1]);
-            }
-            if(ir.m == 13){
-                sp--;
-                stack[sp] = (stack[sp] >= stack[sp+1]);
-            }
-
-        }
-
-        else if(ir.op == 3){
-            sp++;
-            stack[sp] = stack[base(ir.l, bp) + ir.m];
-        }
-
-        else if(ir.op == 4){
-            printf("one");
-            stack[base(ir.l, bp) + ir.m] = stack[sp];
-            sp = sp--;
-            printf("two");
-        }
-
-        else if(ir.op == 5){
-            stack[sp+1] = 0;
-            stack[sp+2] = base(ir.l, bp);
-            stack[sp+3] = bp;
-            stack[sp+4] = pc;
-            bp = sp+1;
-            pc = ir.m;
-        }
-
-        else if(ir.op == 6){
-            sp = sp+ir.m;
-        }
-
-        else if(ir.op == 7){
-            pc = ir.m;
-        }
-
-        else if(ir.op == 8){
-            if(stack[sp] == 0)
-                pc = ir.m;
-
-            sp--;
-        }
-
-        else if(ir.op == 9){
-            if(ir.l == 0){
-                printf("%d", stack[sp]);
-                sp--;
-            }
-            if(ir.l == 1){
-                sp++;
-                scanf("%d", &stack[sp]);
-            }
-            if(ir.l == 2){
-                halt = 1;
-            }
-        }
-
-        printf("%d\t %d\t %d\n", pc, bp, sp);
-
-        counterB++;
-    }
+    printf("\nExecution: \n");
+    printf("\t\t\t pc\t  bp\t  sp\tstack\n", pc, bp, sp);
+    printf("\t\t\t %2d\t  %2d\t  %2d\n", pc, bp, sp);
+    execute(instructions, stack, pc, bp, sp);
 
     return 0;
 }
 
-int base(int l,int base,int *stack){
+int base(int l, int base, int stack[]){
     int b1=base;
     while(l>0){
-        b1=stack[b1+l];
+        b1=stack[b1+1];
         l--;
     }
 
     return b1;
 }
 
-instruction fetch(int instructions[][3], int counter){
+instruction fetch(int instructions[][4], int counter){
     instruction ir;
 
     ir.op = instructions[counter][0];
@@ -248,42 +128,32 @@ char* getOpCode(int op, int M){
     return commands[op-1];
 }
 
-void translate(int instructions[][3], char* commands, int counter){
-    int i;
-    for(i=0; i<counter; i++){
-        printf("%d  ", i);
-        if(instructions[i][0] == 9){
-            if(instructions[i][2] == 0)
-                printf("%s      ", commands[8]);
-            if(instructions[i][2] == 1)
-                printf("%s      ", commands[9]);
-            if(instructions[i][2] == 2)
-                printf("%s      ", commands[10]);
-        }
-        if(instructions[i][0] == 4){
-            printf("%s      ", commands[4]);
-            printf("%d", instructions[i][1]);
-        }
-        if(instructions[i][0] == 5){
-            printf("%s      ", commands[5]);
-            printf("%d", instructions[i][1]);
-        }
+void execute(int instructions[][4], int stack[MAX_STACK_HEIGHT], int pc, int bp, int sp){
 
-
-    }
-
-}
-
-/*void execute(int instructions[][3], int stack[MAX_STACK_HEIGHT], int pc, int bp, int sp){
-    int counter, halt=0;
+    int halt = 0;
+    int cal = -1;
+    char* name;
     instruction ir;
 
     while(halt != 1)
     {
-        ir = fetch(instructions, counter);
+        ir = fetch(instructions, pc);
 
+        name = getOpCode(instructions[pc][0], instructions[pc][2]);
+        printf("%3d  %s\t", instructions[pc][3], name);
+
+        if(name == "STO" || name == "CAL" || name == "LOD")
+            printf("    %d", instructions[pc][1]);
+        if(name != "RET" && name != "HLT")
+            printf("\t%3d", instructions[pc][2]);
+        else
+            printf("\t");
+
+        pc = pc+1;
+
+        //Actually performing the operations:
         if(ir.op == 1){
-            sp++;
+            sp = sp+1;
             stack[sp] = ir.m;
         }
 
@@ -298,63 +168,63 @@ void translate(int instructions[][3], char* commands, int counter){
                 stack[sp] = -stack[sp];
             }
             if(ir.m == 2){
-                sp--;
+                sp = sp-1;
                 stack[sp] = stack[sp]+stack[sp+1];
             }
             if(ir.m == 3){
-                sp--;
+                sp = sp-1;
                 stack[sp] = stack[sp]-stack[sp+1];
             }
             if(ir.m == 4){
-                sp--;
+                sp = sp-1;
                 stack[sp] = stack[sp]*stack[sp+1];
             }
             if(ir.m == 5){
-                sp--;
+                sp = sp-1;
                 stack[sp] = stack[sp]/stack[sp+1];
             }
             if(ir.m == 6){
                 stack[sp] = stack[sp]%2;
             }
             if(ir.m == 7){
-                sp--;
+                sp = sp-1;
                 stack[sp] = stack[sp]%stack[sp+1];
             }
             if(ir.m == 8){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] == stack[sp+1]);
             }
             if(ir.m == 9){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] != stack[sp+1]);
             }
             if(ir.m == 10){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] < stack[sp+1]);
             }
             if(ir.m == 11){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] <= stack[sp+1]);
             }
             if(ir.m == 12){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] > stack[sp+1]);
             }
             if(ir.m == 13){
-                sp--;
+                sp = sp-1;
                 stack[sp] = (stack[sp] >= stack[sp+1]);
             }
 
         }
 
         else if(ir.op == 3){
-            sp++;
+            sp = sp+1;
             stack[sp] = stack[base(ir.l, bp, stack) + ir.m];
         }
 
         else if(ir.op == 4){
             stack[base(ir.l, bp, stack) + ir.m] = stack[sp];
-            sp = sp--;
+            sp = sp-1;
         }
 
         else if(ir.op == 5){
@@ -364,6 +234,7 @@ void translate(int instructions[][3], char* commands, int counter){
             stack[sp+4] = pc;
             bp = sp+1;
             pc = ir.m;
+            cal = sp;
         }
 
         else if(ir.op == 6){
@@ -378,25 +249,40 @@ void translate(int instructions[][3], char* commands, int counter){
             if(stack[sp] == 0)
                 pc = ir.m;
 
-            sp--;
+            sp = sp-1;
         }
 
         else if(ir.op == 9){
-            if(ir.l == 0){
+            if(ir.m == 0){
                 printf("%d", stack[sp]);
-                sp--;
+                sp = sp-1;
             }
-            if(ir.l == 1){
-                sp++;
+            if(ir.m == 1){
+                sp = sp+1;
                 scanf("%d", &stack[sp]);
             }
-            if(ir.l == 2){
+            if(ir.m == 2){
                 halt = 1;
             }
         }
 
-        counter++;
+        //Printing out pc, bp, sp, and stack.
+        printf("\t%3d\t %3d\t %3d\t", pc, bp, sp);
+        if(name != "JMP")
+            printStack(stack, sp, cal);
+        printf("\n");
+
     }
 
 }
-*/
+
+void printStack(int stack[], int sp, int cal){
+    int i;
+    for(i=0; i<sp; i++){
+        printf("%d ", stack[i]);
+        if(cal != -1)
+            if(i==cal)
+            printf("| ");
+    }
+    //printf("\n");
+}
